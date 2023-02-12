520486a6a954eeeea8049e8b1d6746952f1ecdf8
maltempoLuca <maltempoluca98@gmail.com>
2023-02-12 17:23:07 +0100

    Aggiunto middleware per la gestione di errori lato server. Attraverso questo codice il BE non dovrebbe mai bloccarsi ma al massimo restituire un errore al FE

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
-//region imports
+//region Imports
 import {initRoutes} from "./routes/init-routes";
 import {Request, Response} from "express";
+import * as winston from "winston";
+
 
 const express = require('express');
 const path = require('path');
@@ -10,13 +12,26 @@ const database = require('./database');
 const https = require('https');
 const fs = require('fs');
 const helmet = require('helmet');
+const morgan = require('morgan');
 // endregion
 
 const options = {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
 };
+const data = new Date().toISOString().substr(0, 10);
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.prettyPrint()
+    ),
+    transports: [
+        new winston.transports.File({filename: `./log/${data}.log`})
+    ]
+});
 const app = express();
+// morgan è un console logger, con l'opzione tiny loggerà in console tutte le chiamate che arriveranno al server e lo stato della chiamata
+app.use(morgan('tiny'));
 const server = https.createServer(options, app);
 
 
@@ -26,6 +41,14 @@ app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+
+// Middleware degli errori
+app.use((err: any, req: Request, res: Response, next: any) => {
+    logger.info(`richiesta ${req.path}`);
+    console.log(JSON.stringify(err));
+    logger.error(`richiesta "${req.path}" errore: ${JSON.stringify(err)}, parametri: ${req.body.toString()}`);
+    return res.status(err.statusCode).send({error: {...err}});
+});
 
 // import routes into app
 initRoutes(app);
@@ -40,8 +63,13 @@ app.use((req: Request, res: Response) => {
  * NON CAMBIARE FORCE A TRUE, SE LO CAMBI DISTRUGGI TUTTI I DATI PRESENTI NEL DB
  */
 database.sync({alter: true, force: false}).then(() => {
-    console.log('server running');
     server.listen(process.env.PORT || 3000);
+    logger.info(`server Back-End in ambiente ${process.env.ENVIRONMENT} protocollo ${process.env.PROTOCOL} porta ${process.env.PORT_BACKEND}`);
 }).catch((err: Error) => {
     console.log(err);
 })
+
+// Prevengo errori non gestiti che possono bloccare l'esecuzione del server
+process.on('uncaughtException', (err) => {
+    logger.error(err.message);
+});
--- controllers/auth-controller.ts
+++ controllers/auth-controller.ts
@@ -13,13 +13,16 @@ export class AuthController {
             where: {email: user.email}
         }).then((u: UserItem) => {
             if (!u) {
-                throw new Error('user not found!');
+                return res.status(404).json({error: "User not found"});
             }
             bcrypt.compare(user.password, u.password).then((result: boolean) => {
                 if (!result) {
                     throw new Error('wrong password!');
                 }
-                res.json({message: 'user found', user: {id: u.id, name: u.name, lastName: u.lastName}});
+                return res.status(200).json({
+                    message: 'user found',
+                    user: {id: u.id, name: u.name, lastName: u.lastName}
+                });
             });
         })
             .catch((err: Error) => {
