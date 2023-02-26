f5d3294bb7115b6d72ecb2cce7bc1f2f3a92ebb5
maltempoLuca <maltempoluca98@gmail.com>
2023-02-26 10:01:24 +0100

    modificate stringhe di connessione -> db locale

--- src/database.ts
+++ src/database.ts
@@ -4,20 +4,20 @@ const cert = fs.readFileSync('server.cert');
 export const db = require('knex')({
     client: 'mysql',
     connection: {
-        // host: 'localhost',
-        // database: 'gymhub_local',
-        // user: 'root',
-        // password: 'rootpsw',
-        // port: 3306
-        host: process.env.DB_HOST,
-        database: process.env.DB_CONNECTION_NAME,
-        user: process.env.DB_USER_NAME,
-        password: process.env.DB_PASSWORD,
+        host: 'localhost',
+        database: 'gymhub_test',
+        user: 'root',
+        password: 'rootpsw',
         port: 3306,
-        ssl: {
-            key: key,
-            cert: cert,
-        }
+        // host: process.env.DB_HOST,
+        // database: process.env.DB_CONNECTION_NAME,
+        // user: process.env.DB_USER_NAME,
+        // password: process.env.DB_PASSWORD,
+        // port: 3306,
+        // ssl: {
+        //     key: key,
+        //     cert: cert,
+        // }
     }
 });
 
