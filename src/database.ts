6a6a2714cf2de2eb0f8ea988572fb89c0e7c78c4
Alessandro <alessandro.manca910@gmail.com>
2023-03-04 15:23:30 +0100

    -ritornato all'altro db

--- src/database.ts
+++ src/database.ts
@@ -1,24 +1,23 @@
 const fs = require('fs');
-/*const key = fs.readFileSync('server.key');
-const cert = fs.readFileSync('server.cert');*/
+const key = fs.readFileSync('server.key');
+const cert = fs.readFileSync('server.cert');
 export const db = require('knex')({
     client: 'mysql',
     connection: {
-        host: 'localhost',
+        /*host: 'localhost',
         database: 'gymhub_local',
         user: 'root',
         password: 'Alessandro12',
-        port: 3306
-        /*      host: process.env.DB_HOST,
-              database: process.env.DB_CONNECTION_NAME,
-              user: process.env.DB_USER_NAME,
-              password: process.env.DB_PASSWORD,
-              port: 3306,
-              ssl: {
-                  key: key,
-                  cert: cert,
-              }
-         */
+        port: 3306*/
+        host: process.env.DB_HOST,
+        database: process.env.DB_CONNECTION_NAME,
+        user: process.env.DB_USER_NAME,
+        password: process.env.DB_PASSWORD,
+        port: 3306,
+        ssl: {
+            key: key,
+            cert: cert,
+        }
     }
 });
 
