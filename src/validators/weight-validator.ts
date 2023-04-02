1f61599ce9d9146250d4d9002545d721989feb28
maltempoLuca <maltempoluca98@gmail.com>
2023-04-02 09:44:56 +0200

    fine prove

--- src/validators/weight-validator.ts
+++ src/validators/weight-validator.ts
@@ -3,18 +3,6 @@ const yup = require('yup');
 module.exports = yup.object().shape({
     weight: yup.number().required().min(0, "Weight must be a positive number"),
     date: yup.date().required().transform((data: Date) => {
-
-
-        console.log('original date', data);
-        data.setHours(0, 0, 0, 0);
-        console.log('date 0000', data);
-        console.log('formatted date 0000', new Date(data));
-        console.log('new date', new Date());
-
-        console.log('confronti --------------');
-        console.log(data < new Date());
-
-
         return data;
     }).max(new Date(), "Cannot use future date"),
     createdAt: yup.date().default(() => new Date()),
