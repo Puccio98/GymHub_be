da1b5e3908ba16c05d9845c0d00ad0fbe3e0c5d1
maltempoLuca <maltempoluca98@gmail.com>
2023-04-02 09:49:10 +0200

    prove pesi

--- src/validators/weight-validator.ts
+++ src/validators/weight-validator.ts
@@ -3,7 +3,6 @@ const yup = require('yup');
 module.exports = yup.object().shape({
     weight: yup.number().required().min(0, "Weight must be a positive number"),
     date: yup.date().required().transform((data: Date) => {
-        data.setHours(0, 0, 0, 0);
 
 
         console.log('original date', data);
