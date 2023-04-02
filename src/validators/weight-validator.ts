b606e9fce7927f00034200697f80f4759af23cbd
maltempoLuca <maltempoluca98@gmail.com>
2023-04-02 10:40:54 +0200

    ritorna data correttamente

--- src/validators/weight-validator.ts
+++ src/validators/weight-validator.ts
@@ -3,7 +3,8 @@ const yup = require('yup');
 module.exports = yup.object().shape({
     weight: yup.number().required().min(0, "Weight must be a positive number"),
     date: yup.date().required().transform((data: Date) => {
-        return data.setUTCHours(0, 0, 0, 0);
+        data.setUTCHours(0, 0, 0, 0);
+        return data;
     }).max(new Date(), "Cannot use future date"),
     createdAt: yup.date().default(() => new Date()),
     updatedAt: yup.date().default(() => new Date())
