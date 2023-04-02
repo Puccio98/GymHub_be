ecd9358ac204fd370813f75557d41478fec39fd7
maltempoLuca <maltempoluca98@gmail.com>
2023-04-02 10:27:02 +0200

    ora setta l'orario UTC

--- src/validators/weight-validator.ts
+++ src/validators/weight-validator.ts
@@ -3,7 +3,7 @@ const yup = require('yup');
 module.exports = yup.object().shape({
     weight: yup.number().required().min(0, "Weight must be a positive number"),
     date: yup.date().required().transform((data: Date) => {
-        return data.setHours(0, 0, 0, 0);
+        return data.setUTCHours(0, 0, 0, 0);
     }).max(new Date(), "Cannot use future date"),
     createdAt: yup.date().default(() => new Date()),
     updatedAt: yup.date().default(() => new Date())
