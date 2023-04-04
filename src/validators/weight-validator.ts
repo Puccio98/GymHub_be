fab4e90ccdc52c4552a3444812bfae06b1917f0a
Alessandro <alessandro.manca910@gmail.com>
2023-04-04 12:24:19 +0200

    -commentato setHours

--- src/validators/weight-validator.ts
+++ src/validators/weight-validator.ts
@@ -3,10 +3,10 @@ const yup = require('yup');
 module.exports = yup.object().shape({
     weight: yup.number().required().min(0, "Weight must be a positive number"),
     date: yup.lazy(() => {
-        return yup.date().required().transform((data: Date) => {
+        return yup.date().required()/*.transform((data: Date) => {
             data.setHours(0, 0, 0, 0);
             return data;
-        }).max(new Date(), "Cannot use future date")
+        })*/.max(new Date(), "Cannot use future date")
     }),
     createdAt: yup.date().default(() => new Date()),
     updatedAt: yup.date().default(() => new Date())
