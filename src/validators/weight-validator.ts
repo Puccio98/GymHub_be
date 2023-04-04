27cf762c4050ae443f21be412321927f2fb1c9d2
Alessandro <alessandro.manca910@gmail.com>
2023-04-04 09:06:41 +0200

    -funziona, ho rimosso i console log inutili

--- src/validators/weight-validator.ts
+++ src/validators/weight-validator.ts
@@ -4,15 +4,7 @@ module.exports = yup.object().shape({
     weight: yup.number().required().min(0, "Weight must be a positive number"),
     date: yup.lazy(() => {
         return yup.date().required().transform((data: Date) => {
-            console.log('data originale: ' + data);
             data.setHours(0, 0, 0, 0);
-            console.log('data settata a 0: ' + data);
-            console.log('data corrente server: ' + new Date());
-
-            const result: boolean = data < new Date();
-
-            console.log('data è minore di new Date()? ' + result.toString());
-
             return data;
         }).max(new Date(), "Cannot use future date")
     }),
