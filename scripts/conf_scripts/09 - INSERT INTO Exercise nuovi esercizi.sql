c787710d34d51a2a8f1ad826e700ff0bdd421fb0
Alessandro <alessandro.manca910@gmail.com>
2023-04-08 15:34:48 +0200

    -aggiunto script per i nuovi esercizi

---
+++ scripts/conf_scripts/09 - INSERT INTO Exercise nuovi esercizi.sql
@@ -0,0 +1,23 @@
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Back Extension', now(), now());
+
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Lunges', now(), now());
+
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Hip Thrust', now(), now());
+
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Face Pull', now(), now());
+
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Leg Press', now(), now());
+
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Leg Extension', now(), now());
+
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Military Press', now(), now());
+
+INSERT INTO Exercise(Name, createdAt, updatedAt)
+VALUES('Pendlay Row', now(), now());
