a68b2a550bde4116c51b3b32532c7ceeb1d37bfe
Alessandro <alessandro.manca910@gmail.com>
2023-04-12 19:02:24 +0200

    -creato enum che rappresenta lo status di esercizi, workout e schede

--- src/dto/programDto/program-dto.ts
+++ src/dto/programDto/program-dto.ts
@@ -1,5 +1,6 @@
 import {ProgramStateEnum} from "../../enums/program-state-enum";
 import {WorkoutDto} from "./workout-dto";
+import {ExerciseStatus} from "../../enums/exercise-status.enum";
 
 export interface ProgramDto {
     programID?: number,
@@ -9,7 +10,7 @@ export interface ProgramDto {
     programStateID: ProgramStateEnum,
     numberOfWorkout?: number
     workoutList: WorkoutDto[],
-    statusID: number
+    statusID: ExerciseStatus
 }
 
 export {}
---
+++ src/enums/exercise-status.enum.ts
@@ -0,0 +1,5 @@
+export enum ExerciseStatus {
+  INCOMPLETE = 1,
+  COMPLETE = 2,
+  SKIPPED = 3
+}
