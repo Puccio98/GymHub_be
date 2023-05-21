dc782b10a4b01184d56fe5860c8ac74f903d9cef
maltempoLuca <maltempoluca98@gmail.com>
2023-05-21 17:26:37 +0200

    Aggiunta funzione per ricercare alimento tramite nome

--- src/dao/food-dao.ts
+++ src/dao/food-dao.ts
@@ -4,25 +4,34 @@ import {itemUSDA, USDA_Dto} from "../dto/externalApiDto/USDA/usda-dto";
 import {FoodItem} from "../models/food";
 
 const usda_key: string = 'EoYBMJ5Trk0D5qe40cCewrHdeWuagtPMGg8sB3wq';
-const usda_server: string = 'https://api.nal.usda.gov/fdc'
-const esempio_chiamata_usda_barcode: string = 'https://api.nal.usda.gov/fdc/v1/foods/search'
-//const off = require('openfoodfacts-nodejs');
-//const client = new off();
 const axios = require("axios");
 
-
 export class FoodDao {
-    // region Public Methods
-    //find by name
+    /***
+     * Restituisce una lista di alimenti in base alla descrizione
+     * @param description
+     */
+    static async search(description: string): Promise<FoodItem[]> {
+        const foods: FoodItem[] = await db('Food')
+            .where('description', 'like', `%${description}%`)
+            .select();
+        return foods;
+    }
 
 
-    // get by barcode
+    /***
+     * Restituisce l'alimento in DB tramite codice a barre.
+     * @param barcode
+     */
     static async get(barcode: string): Promise<FoodItem> {
         const foods: FoodItem[] = await db('Food').where({Barcode: barcode}).select();
         return foods[0];
     }
 
-    // Ricerca il codice a barre sul database di open food facts
+    /**
+     * Ricerca un alimento sul DB di Open Food Facts tramite codice a barre
+     * @param barcode
+     */
     static async getFromOFF(barcode: string): Promise<itemOFF | null> {
         const getParams: string = '?fields=product_name,product_name_it,nutriments,product_quantity,quantity,serving_size,categories_old';
         const config: any = {headers: {'User-Agent': 'GymHub - Version 1.0'}};
@@ -38,7 +47,10 @@ export class FoodDao {
         }
     }
 
-    // Ricerca il codice a barre sul database di USDA FoodData Central
+    /**
+     * Ricerca un alimento sul DB di USDA tramite codice a barre
+     * @param barcode
+     */
     static async getFromUSDA(barcode: string): Promise<itemUSDA | null> {
         const getParams: string = `?query=${barcode}&pageSize=1&api_key=${usda_key}`;
         try {
@@ -53,7 +65,10 @@ export class FoodDao {
         }
     }
 
-    // Verifica che un alimento abbia i campi necessari per l'inserimento
+    /**
+     * Verifica che un alimento abbia tutti i campi necessari per essere inserito in DB
+     * @param food
+     */
     static checkCreate(food: FoodItem): boolean {
         // Nome deve essere valorizzato
         if (!food.Description) {
@@ -70,11 +85,14 @@ export class FoodDao {
         return true;
     }
 
+    /***
+     * Inserisce Food Item in DB
+     * @param userFood
+     */
     static async create(userFood: FoodItem): Promise<number> {
         const f = await db('Food').insert(userFood);
         // Ritorno l'id del record appena creato
         return f[0];
     }
 
-    //endregion
 }
--- src/routes/nutrition-routes.ts
+++ src/routes/nutrition-routes.ts
@@ -4,7 +4,9 @@ import {NutritionController} from "../controllers/nutrition-controller";
 module.exports = (app: { use: (arg0: string, arg1: any) => void; }) => {
     const router = require("express").Router();
 
-    router.get('/food/:barcode', NutritionController.food);
+    router.get('/food/barcode/:barcode', NutritionController.getFood);
+
+    router.get('/food/description/:description', NutritionController.searchFoods);
 
     app.use('/nutrition', router);
 };
