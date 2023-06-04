cd84beefb25e55dfbcbf2bf971ca5abaf34f5215
NITRO5\aless <alessandro.manca910@gmail.com>
2023-06-04 15:27:54 +0200

    cambiato basefood dto

--- src/models/food_user.ts
+++ src/models/food_user.ts
@@ -1,7 +1,8 @@
 export interface BaseFood_UserItem {
     FoodID: number,
     MealID: number,
-    UserID: number
+    UserID: number,
+    Quantity: number
 }
 
 export interface Food_UserItem extends BaseFood_UserItem {
@@ -9,7 +10,6 @@ export interface Food_UserItem extends BaseFood_UserItem {
      * Id in inserimento è un campo nullo.
      */
     Food_UserID?: number,
-    Quantity: number,
     Date: string,
     createdAt: Date,
     updatedAt: Date
