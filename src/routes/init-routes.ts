5ea8c4a6cc277bab010b441f56ee4b7035948e89
maltempoLuca <maltempoluca98@gmail.com>
2023-06-25 20:57:51 +0200

    Eliminate rotte superflue

--- src/routes/init-routes.ts
+++ src/routes/init-routes.ts
@@ -5,8 +5,6 @@ export function initRoutes(app: Express) {
     require("./nutrition-routes")(app);
     require("./program-routes")(app);
     require("./weight-routes")(app);
-    require("./workout-routes")(app);
     require("./exercise-routes")(app);
-    require("./exercise-workout-routes")(app);
     require("./user-routes")(app);
 }
