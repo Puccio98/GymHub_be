3e3ce59c63d3f9d2e5b7b0c412933a0fd50818e6
maltempoLuca <maltempoluca98@gmail.com>
2023-06-25 19:37:42 +0200

    Rinominate chiamate program secondo schema REST

--- src/routes/program-routes.ts
+++ src/routes/program-routes.ts
@@ -7,20 +7,19 @@ import {shareProgramType} from "../validators/share-program-validator"
 module.exports = (app: { use: (arg0: string, arg1: any) => void; }) => {
     const router = require("express").Router();
 
-    router.get('/program_get', ProgramController.getListByUserID);
+    router.get('', ProgramController.getListByUserID);
 
-    router.post('/program_create', validateDto(createProgramType), ProgramController.create);
+    router.post('', validateDto(createProgramType), ProgramController.create);
+
+    router.patch('/:program_id', validateDto(editProgramType), ProgramController.edit);
 
     router.delete('/:program_id', ProgramController.delete);
 
     router.patch('/:program_id/reset', ProgramController.reset);
 
-    router.patch('/:program_id', validateDto(editProgramType), ProgramController.edit);
-
     router.post('/share', validateDto(shareProgramType), ProgramController.share);
 
     router.get('/shared/user/:user_id', ProgramController.getShared);
-
 
     app.use('/program', router);
 };
